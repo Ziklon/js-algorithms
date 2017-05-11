d3ade6483f918d998e7706a2c6c5b069733ed550
Torres Cristobal, Wilber <wilber.torres@one.verizon.com>
2017-05-10 19:21:46 -0500

    removing not used methods

--- src/data-structures/linked-list.js
+++ src/data-structures/linked-list.js
@@ -113,20 +113,4 @@
   };
 
 
-  /**
-   * Method to sum 2 numbers
-   * @module adhoc/sum
-   * @param {a} number - Number to sum.
-   * @param {b} number - Number to sum.
-   * @returns {Number} return the sum of a + b
-   *
-   * @example
-   * var sum = require('your_path/src/sum').sum;
-   *
-   * console.log(sum(1,2)); // 3
-   */
-  exports.sum = function (a, b) {
-    return a + b;
-  };
-
 }(typeof exports === 'undefined' ? window : exports));
